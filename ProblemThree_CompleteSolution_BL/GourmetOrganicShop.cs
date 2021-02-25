7d7643f60cd50058a7291d1435e7b21dd9e1185a
BhanuSri_Madeti <78855795+99003665@users.noreply.github.com>
2021-02-25 17:35:31 +0530

    Update GourmetOrganicShop.cs

--- ProblemThree_CompleteSolution_BL/GourmetOrganicShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetOrganicShop.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +8,42 @@ namespace ProblemThree_CompleteSolution_BL
 {
     public class GourmetOrganicShop : GourmetShop,ITaxCompute
     {
-        
+
+        var organicPriceDict = new Dictionary<int, double>();
+
+        int pieceprice = 100;
+        public void PayPerPiece(int quantity)
+        {
+            if (quantity > 2)
+            {
+                organicPriceDict = pieceprice * quantity;
+                return organicPriceDict;
+            }
+
+
+        }
+
+        public void additems()
+        {
+
+            oitems[] obj = new oitems[20];
+            //for electronics
+            for (int i = 0; i < 20; i++)
+            {
+
+                itemobj[0] = new oitems(1, 70");
+                itemobj[1] = new oitems(2, 100);
+                
+
+               oitems.Add(itemobj[i]);
+
+            }
+
+
+
+
+
+
+        }
     }
 }
