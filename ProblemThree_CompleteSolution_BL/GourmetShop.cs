76a11c45f95020c1827ba17b81b514774066eb6a
BhanuSri_Madeti <78855795+99003665@users.noreply.github.com>
2021-02-25 17:35:07 +0530

    Update GourmetShop.cs

--- ProblemThree_CompleteSolution_BL/GourmetShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetShop.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +7,24 @@ using System.Threading.Tasks;
 namespace ProblemThree_CompleteSolution_BL
 {
     public abstract class GourmetShop
-    {          
+    {
         public abstract double PayPerPiece(int quantity);
         public abstract double PayPerWeight(int weight);
 
     }
+    public int Itemid=0;
+    public String ItemName = null;
+    
+    public int price = 0;
+
+    public GourmetShop(int Itemid, String ItemName, int price)
+    {
+        this.Itemid = Itemid;
+        this.ItemName = ItemName;
+        this.price = price;
+       
+    }
+
+    public static List<Gourmett> Mainitems { get; set; } = new List<GourmetShop>();
+
 }
