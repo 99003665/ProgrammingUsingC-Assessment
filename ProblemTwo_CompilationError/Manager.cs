5c0f407e5128d917775c4cd2ee965465312a034c
BhanuSri_Madeti <78855795+99003665@users.noreply.github.com>
2021-02-25 16:33:59 +0530

    Update Manager.cs

--- ProblemTwo_CompilationError/Manager.cs
+++ ProblemTwo_CompilationError/Manager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProblemTwo_CompilationError
 {
-    class Manager
+    class Manager : Employee
     {
         public List<Employee> DirectReports { get; set; }
         public string CostCode { get; set; }
