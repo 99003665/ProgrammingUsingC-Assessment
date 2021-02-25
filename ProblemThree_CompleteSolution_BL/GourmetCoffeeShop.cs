b9173d37d8fc6eee4c62a1a1d5e7b7564a4d9c7c
BhanuSri_Madeti <78855795+99003665@users.noreply.github.com>
2021-02-25 17:36:12 +0530

    Update GourmetCoffeeShop.cs

--- ProblemThree_CompleteSolution_BL/GourmetCoffeeShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetCoffeeShop.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +8,39 @@ namespace ProblemThree_CompleteSolution_BL
 {
     public class GourmetCoffeeShop : GourmetShop, ITaxCompute
     {
-        
+        public GourmetCoffeeShop(int icode,string iname,Dictionary<int,double>iPrice)
+        {
+            var CoffeePriceDict = new Dictionary<int, double>();
+            int pieceprice = 100;
+            public void PayPerPiece(int quantity)
+            {
+                if (quantity > 2)
+                {
+                    CoffeePriceDict = pieceprice * quantity;
+                    return CoffeePriceDict;
+                }
+
+
+            }
+
+        }
+        public class Totalprice
+        {
+            public int Total(int quantity)
+            {
+
+                if (quantity > 20)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+
+
+            }
+        }
+
     }
 }
